2473d26e701c651cfd68f07a3b29c91d94bbbf52
higuchi-learn <haruki.higuchi.000@gmail.com>
2025-12-15 17:44:38 +0900

    07

---
+++ app/Http/Controllers/DiaryController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DiaryController extends Controller
+{
+    //
+    public function index()
+    {
+        $name = 'Laravel';
+        return view('diary.index', ['name' => $name]);
+    }
+}
---
+++ database/migrations/2025_12_15_172646_create_diaries_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    protected $table = 'diaries';
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('diaries', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->string('title');
+            $table->text('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('diaries');
+    }
+};
